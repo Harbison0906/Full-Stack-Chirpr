39553db0b33a07aed53272a9df7d4e1233335c00
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-06-30 21:35:34 -0500

    commit

--- src/server/db/queries/chirps.ts
+++ src/server/db/queries/chirps.ts
@@ -4,6 +4,6 @@ import {Query} from '../';
 const all = () => Query('SELECT * FROM chirps');
 const one = (id: number) => Query('SELECT * FROM chirps WHERE id = ?', [id]);
 const destroy = (id: number) => Query('DELETE FROM chirps WHERE id = ?', [id])
-const update = ()
 
-export default {all, one, destroy, update};
+
+export default {all, one, destroy};
