77d63421074806d934f65c2d798a1359cee70c14
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-07-01 14:18:23 -0500

    added DELETE request route to delete a chirp

--- src/server/routes/chirps.ts
+++ src/server/routes/chirps.ts
@@ -20,4 +20,15 @@ router.get('/:id?', async (req, res, next) => {
   }
 });
 
+router.delete('/:id?', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const deleteChirp = await db.chirps.destroy(id);
+    res.json('Successfully deleted!');
+  } catch (error) {
+    console.log(error)
+    res.status(500).json('Your chirp could not be deleted.')
+  }
+})
+
 export default router;
